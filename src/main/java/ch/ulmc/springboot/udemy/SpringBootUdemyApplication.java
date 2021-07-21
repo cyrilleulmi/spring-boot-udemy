420f630bcc33a2a45faaf5250c3bd70753547cd5
ulmc <cyrille.ulmi@nis.ch>
2021-07-21 15:25:52 +0200

    moved files

--- src/main/java/ch/ulmc/springboot/udemy/SpringBootUdemyApplication.java
+++ src/main/java/ch/ulmc/springboot/udemy/SpringBootUdemyApplication.java
@@ -1,4 +1,4 @@
-package ch.ulmc.springboot.udemy.springbootudemy;
+package ch.ulmc.springboot.udemy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ch/ulmc/springboot/udemy/SpringBootUdemyApplicationTests.java
+++ src/test/java/ch/ulmc/springboot/udemy/SpringBootUdemyApplicationTests.java
@@ -1,4 +1,4 @@
-package ch.ulmc.springboot.udemy.springbootudemy;
+package ch.ulmc.springboot.udemy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

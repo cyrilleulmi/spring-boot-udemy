b1597c0381fa42d52a1dd5e3b825cb6d9378c6c5
ulmc <cyrille.ulmi@nis.ch>
2021-07-21 14:18:36 +0200

    initialized spring boot project

---
+++ src/main/java/ch/ulmc/springboot/udemy/springbootudemy/SpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package ch.ulmc.springboot.udemy.springbootudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/ulmc/springboot/udemy/springbootudemy/SpringBootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ulmc.springboot.udemy.springbootudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

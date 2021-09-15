e1a4acd43cac3f23f8e5ff75990ac5d601b97c9e
Cyrille Ulmi <cyrille.ulmi@bluewin.ch>
2021-09-15 15:39:09 +0200

    added users api and connected it to eureka server

---
+++ users-api/users-api/src/test/java/ch/ulmc/springboot/api/users/usersapi/UsersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ulmc.springboot.api.users.usersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

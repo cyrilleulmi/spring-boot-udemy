d7827639b3867081b1f80e85b9544c13df60de55
Cyrille Ulmi <cyrille.ulmi@bluewin.ch>
2021-09-23 14:55:02 +0200

    created java project for spring api gateway

---
+++ api-gateway/src/test/java/ch/ulmc/springboot/api/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ulmc.springboot.api.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

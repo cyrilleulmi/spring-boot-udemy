d19294d6d20ac63ed1d8123eabf688fc22919034
Cyrille Ulmi <cyrille.ulmi@bluewin.ch>
2021-10-27 15:49:25 +0200

    added spring security and password encryption

--- users-api/users-api/src/main/java/ch/ulmc/springboot/api/users/usersapi/ui/controllers/UsersService.java
+++ users-api/users-api/src/main/java/ch/ulmc/springboot/api/users/usersapi/ui/controllers/UsersService.java
@@ -5,20 +5,23 @@ import java.util.UUID;
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersService {
     UsersRepository usersRepository;
+    BCryptPasswordEncoder passwordEncoder;
 
-    public UsersService(UsersRepository usersRepository) {
+    public UsersService(UsersRepository usersRepository, BCryptPasswordEncoder passwordEncoder) {
         this.usersRepository = usersRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserEntity createUser(@Valid CreateUserRequest userData) {
         ModelMapper modelMapper = new ModelMapper();
         UserEntity userEntity = modelMapper.map(userData, UserEntity.class);
-        userEntity.setEncryptedPassword("hardcoded_password");
+        userEntity.setEncryptedPassword(passwordEncoder.encode(userData.getPassword()));
         userEntity.setUserId(UUID.randomUUID().toString());
         usersRepository.save(userEntity);
         return userEntity;
